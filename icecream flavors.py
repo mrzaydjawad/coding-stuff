17ea656f37e29901b6d340111175a8881cf1724e
mrzaydjawad <zaydzia@gmail.com>
2022-09-30 07:01:40 +0500

    yes

--- icecream flavors.py
+++ icecream flavors.py
@@ -9,16 +9,22 @@ fv = int(input("pls enter your favorite icecream flavor"))
 while fv !=0:
     if fv == 1:
         print("chocolate")
+        break
     elif fv == 2:
         print("strawberry")
+        break
     elif fv == 3:
         print("vanilla")
+        break
     else:
         print("pls enter a valid icecream flavor")
-    
+        break
+
+print()
 menu()
 fv = int(input("pls enter your favorite icecream flavor"))
 
+print()
 print("thank you for doing the survey")
         
 
