e081a0498c91074b50950e29e815d96461b633a2
mrzaydjawad <zaydzia@gmail.com>
2022-10-03 20:55:39 +0500

    yes

--- icecream.py
+++ icecream.py
@@ -11,21 +11,30 @@ def main():
     while fv != 0:
         if fv == 1:
             print("chocolate")
+            start()
             break
         elif fv == 2:
             print("strawberry")
+            start()
             break
-        elif fv == 3:
+        elif  fv == 3:
             print("vanilla")
+            start()
             break
         else:
             print("pls enter a valid icecream flavor")
+            start()
             break
-    restart = input("do you want to do it again?(Y/N)").lower()
-    if restart in yeslist:
-        main()
-    elif restart in nolist:
-        print("bye have a good day.")
+def start():
+        restart = input("do you want to do it again?(Y/N)").lower()
+        if restart in yeslist:
+            main()
+        elif restart in nolist:
+            print("bye have a good day.")
+        else:
+            print("pls enter yes or no")
+            print()
+            start()
 main()
 
 
