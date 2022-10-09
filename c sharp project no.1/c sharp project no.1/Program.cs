85c19e4fb0680c048c473bd502027f394d08ae3b
mrzaydjawad <zaydzia@gmail.com>
2022-10-09 19:27:13 +0500

    c# go brrrrrrrrrrrr

---
+++ c sharp project no.1/c sharp project no.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_project_no._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = ",just pay a small fee for premium";
+            Console.WriteLine("want a break from the ads " + word);
+            Console.ReadKey();
+
+
+        }
+    }
+}
