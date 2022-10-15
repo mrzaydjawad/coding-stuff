a087024a3bb79250930e8ad39f5482217b547594
mrzaydjawad <zaydzia@gmail.com>
2022-10-15 18:52:13 +0500

    yes

--- python/side project's thingys/cube.py
+++
@@ -1,6 +0,0 @@
-length = input("pls enter the length")
-length = float(length)
-area = length*length*length
-surface_area = length*length*6
-print("the area of the cube is",area)
-print("the total surface area of a cube is",surface_area)
