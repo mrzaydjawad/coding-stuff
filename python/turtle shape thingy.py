987971c341364a0b662e4e3ebf08b93d8774bd57
mrzaydjawad <zaydzia@gmail.com>
2022-10-25 20:08:02 +0500

    yes

--- python/turtle shape thingy.py
+++ python/turtle shape thingy.py
@@ -1,10 +1,42 @@
 import turtle
-turtle.forward(250)
-turtle.shape("circle")
-turtle.shapesize(5)
-turtle.right(270)
-turtle.forward(250)
-turtle.shape("square")
-turtle.forward(100)
-turtle.shape("triangle")
-turtle.forward(250)
+win = turtle.Screen()
+win.title("moving")
+win.bgcolor("black")
+win.setup(width=800, height=800)
+win.tracer(0)
+#moving
+a = turtle.Turtle()
+a.speed(0)
+a.shape("square")
+a.color("white")
+a.shapesize(stretch_wid=5,stretch_len=5)
+a.penup()
+a.goto(0,0)
+#functions
+def a_up():
+    y = a.ycor()
+    y += 20
+    a.sety(y)
+def a_down():
+    y = a.ycor()
+    y -= 20
+    a.sety(y)
+def a_right():
+    x = a.ycor()
+    x += 20
+    a.setx(x)
+def a_left():
+    x = a.ycor()
+    x -= 20
+    a.setx(x)
+
+
+
+#keyboard binding
+win.listen()
+win.onkeypress(a,"Up")
+win.onkeypress(a,"Right")
+win.onkeypress(a,"Down")
+win.onkeypress(a,"Left")
+
+
