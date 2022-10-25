2ea7624a4fe1014e6321585c6c02f7241631adb5
mrzaydjawad <zaydzia@gmail.com>
2022-10-25 20:08:07 +0500

    yes

--- python/turtle shape thingy.py
+++ python/turtle shape thingy.py
@@ -1,42 +1,44 @@
+from re import X
 import turtle
-win = turtle.Screen()
-win.title("moving")
-win.bgcolor("black")
-win.setup(width=800, height=800)
-win.tracer(0)
-#moving
-a = turtle.Turtle()
-a.speed(0)
-a.shape("square")
-a.color("white")
-a.shapesize(stretch_wid=5,stretch_len=5)
-a.penup()
-a.goto(0,0)
-#functions
-def a_up():
-    y = a.ycor()
-    y += 20
-    a.sety(y)
-def a_down():
-    y = a.ycor()
-    y -= 20
-    a.sety(y)
-def a_right():
-    x = a.ycor()
-    x += 20
-    a.setx(x)
-def a_left():
-    x = a.ycor()
-    x -= 20
-    a.setx(x)
 
+def importantmoving():
+    win = turtle.Screen()
+    win.title("moving")
+    win.bgcolor("black")
+    win.setup(width=800, height=800)
+    win.tracer(0)
+    #moving
+    a = turtle.Turtle()
+    a.speed(1)
+    a.shape("square")
+    a.color("white")
+    a.shapesize(stretch_wid=5,stretch_len=3)
+    a.penup()
+    a.goto(0,0)
+    #functions
+    def a_up():
+        y = a.ycor()
+        y += 20
+        a.sety(y)
+    def a_down():
+        y = a.ycor()
+        y -= 20
+        a.sety(y)
+    def a_right():
+        x = a.xcor()
+        x += 20
+        a.setx(x)
+    def a_left():
+        x = a.xcor()
+        x -= 20
+        a.setx(x)
+    #keyboard binding
+    win.listen()
+    win.onkeypress(a_up,"Up")
+    win.onkeypress(a_right,"Right")
+    win.onkeypress(a_down,"Down")
+    win.onkeypress(a_left,"Left")
+    while True:
+        win.update()
 
-
-#keyboard binding
-win.listen()
-win.onkeypress(a,"Up")
-win.onkeypress(a,"Right")
-win.onkeypress(a,"Down")
-win.onkeypress(a,"Left")
-
-
+importantmoving()
