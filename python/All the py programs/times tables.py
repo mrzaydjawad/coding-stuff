65ee402e2685cc335d5dc23b5f1e8146e1ad2d96
mrzaydjawad <zaydzia@gmail.com>
2022-11-06 10:31:41 +0500

    yes

---
+++ python/All the py programs/times tables.py
@@ -0,0 +1,10 @@
+print("in this program you will enter a number and the tables of the number will be shown")
+print()
+number = float(input("pls enter a number:"))
+multi = 1
+a = 1
+while multi is not 12:
+    multi += 1
+    x = number*multi
+    a += 1
+    print(f"{a}--{x}--")
