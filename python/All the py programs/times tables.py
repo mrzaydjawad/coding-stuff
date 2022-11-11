e111966eef5cb0def5dd51ad663d46f2bba421f2
mrzaydjawad <zaydzia@gmail.com>
2022-11-11 07:03:41 +0500

    yes

--- python/All the py programs/times tables.py
+++ python/All the py programs/times tables.py
@@ -3,9 +3,9 @@ print()
 number = float(input("pls enter a number:"))
 multi = 1
 a = 1
-while multi is not 12:
+while multi != 12:
     multi += 1
     x = number*multi
     a += 1
     x = round(x,2)
-    print(f"{a}--{x}--")
+    print(f"{a}~~ {x} ~~")
