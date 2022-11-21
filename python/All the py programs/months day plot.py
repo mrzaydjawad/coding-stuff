a92749aa185a7c887e0bcc3636339aa80c973173
mrzaydjawad <zaydzia@gmail.com>
2022-11-21 20:00:06 +0500

    yes

--- python/All the py programs/months day plot.py
+++ python/All the py programs/months day plot.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
 x = [31,28,31,30,31,30,31,31,30,31,30,31]
-plt.plot(x)
+y = [1,2,3,4,5,6,7,8,9,10,11,12]
+plt.plot(x,y,c="black",linewidth=4,label="days in months")
+plt.xlabel("days in months")
+plt.ylabel("months")
+plt.title("days in each month")
+plt.legend()
 plt.show()
