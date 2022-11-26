3198e60e61571888f7d9ff6aea6d0465398e8efa
mrzaydjawad <zaydzia@gmail.com>
2022-11-26 20:07:26 +0500

    yes

--- python/All the py programs/screensaver.py
+++ python/All the py programs/screensaver.py
@@ -1,3 +1,4 @@
+import time
 import pygame as py
 py.init()
 screen = py.display.set_mode((1920,1080))
@@ -8,6 +9,7 @@ tick = 30
 running = True
 while running:
     screen.blit(bg,(0,0))
+    screen.blit(mover,(890,490))
     py.display.update()
     for event in py.event.get():
         if event.type == py.QUIT:
