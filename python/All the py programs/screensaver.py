9be7f13e69edeeaa40ce53fd08a60430420a8f1f
mrzaydjawad <zaydzia@gmail.com>
2022-11-26 21:01:58 +0500

    yes

--- python/All the py programs/screensaver.py
+++ python/All the py programs/screensaver.py
@@ -1,16 +1,26 @@
-import time
+import random
 import pygame as py
 py.init()
 screen = py.display.set_mode((1920,1080))
 bg = py.image.load('C:/Users/zaydz/Desktop/coding/python/All the py programs/images/space.png')
-mover = py.image.load('C:/Users/zaydz/Desktop/coding/python/All the py programs/images/waterbottles.png')
+mover = py.image.load('C:/Users/zaydz/Desktop/coding/python/All the py programs/images/waterbottles for coding.png')
+mx = random.randrange(200,1720)
+my = random.randrange(203,877)
+sx = 3
+sy = 2
 clock = py.time.Clock()
 tick = 30
 running = True
 while running:
-    screen.blit(bg,(0,0))
-    screen.blit(mover,(890,490))
-    py.display.update()
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
+    screen.blit(bg,(0,0))
+    screen.blit(mover,(mx,my))
+    py.display.update()
+    mx += sx
+    my += sy
+    if mx <= 0 or mx >= 1720 :
+        sx *= -1
+    if my <=0 or my >= 877:
+        sy *= -1
