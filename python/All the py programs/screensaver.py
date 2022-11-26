27493a838c161ffceb1015b0eaf2bef8fbdde6e2
mrzaydjawad <zaydzia@gmail.com>
2022-11-26 19:51:09 +0500

    yes

--- python/All the py programs/screensaver.py
+++ python/All the py programs/screensaver.py
@@ -2,7 +2,7 @@ import pygame as py
 py.init()
 screen = py.display.set_mode((1920,1080))
 bg = py.image.load('C:/Users/zaydz/Desktop/coding/python/All the py programs/images/space.png')
-mover = py.image.load('C:\Users\zaydz\Desktop\coding\python\All the py programs\images\waterbottles.png')
+mover = py.image.load('C:/Users/zaydz/Desktop/coding/python/All the py programs/images/waterbottles.png')
 clock = py.time.Clock()
 tick = 30
 running = True
