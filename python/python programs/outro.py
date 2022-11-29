8817252d87b650e8982a6c7e99a99fcb3e2b7818
mrzaydjawad <zaydzia@gmail.com>
2022-11-29 17:52:09 +0500

    yes

--- python/python programs/outro.py
+++ python/python programs/outro.py
@@ -1,6 +1,6 @@
 import os
 import logging
 from playsound import playsound
-sec = 11
+sec = 13
 os.system(f"shutdown /s /t {sec}")
 playsound("C:\\Users\\zaydz\\Desktop\\coding\\python\\python programs\\sound effects\\outro.mp3")
