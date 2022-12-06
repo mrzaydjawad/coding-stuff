e3318c010c02f932d12d8ea5ebab45de3f6295e1
mrzaydjawad <zaydzia@gmail.com>
2022-12-06 21:22:17 +0500

    yes

--- python/python programs/screensaver.py
+++ python/python programs/screensaver.py
@@ -1,6 +1,5 @@
 import random
 import pygame
-import time
 pygame.init()
 screen = pygame.display.set_mode((1920,1080))
 bg = pygame.image.load('C:/Users/zaydz/Desktop/coding/python/python programs/images/backrooms.jpg')
@@ -12,20 +11,6 @@ mx = random.randrange(400,1520)
 my = random.randrange(200,880)
 sx = 3
 sy = 2
-for i in range(60):
-    seconds += 1
-for i in range (60):
-    if seconds == 60:
-        seconds -= 60
-        minutes += 1
-for i in range(24):
-    if minutes == 60:
-        minutes -= 60
-        hours += 1
-    if hours == 24:
-        seconds -= 60
-        minutes -= 60
-        hours -= 24
 clock = pygame.time.Clock()
 running = True
 while running:
@@ -44,3 +29,18 @@ while running:
         sx *= -1
     if my <=0 or my >= 880:
         sy *= -1
+    for i in range(60):
+        seconds += 1
+    for i in range(60):
+        if seconds == 60:
+            seconds = 0
+            minutes += 1
+    for i in range(24):
+        if minutes == 60:
+            minutes = 0
+            hours += 1
+        if hours == 24:
+            seconds = 0
+            minutes = 0
+            hours = 0
+    clock.tick(1)
