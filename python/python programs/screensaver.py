e8b30bcce98bf68cf08f83cd957fe19d72c08f90
mrzaydjawad <zaydzia@gmail.com>
2022-12-07 17:16:47 +0500

    hmmmm

--- python/python programs/screensaver.py
+++ python/python programs/screensaver.py
@@ -11,36 +11,37 @@ mx = random.randrange(400,1520)
 my = random.randrange(200,880)
 sx = 3
 sy = 2
+current_time = 0
 clock = pygame.time.Clock()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    tume = pygame.font.Font("C:/Users/zaydz/Desktop/coding/python/python programs/fonts/Sitka.ttc",50)
-    tumo = tume.render(f"hour : {hours}  minute : {minutes}  second : {seconds}",False,(237, 205, 187))
-    screen.blit(bg,(0,0))
-    screen.blit(mover,(mx,my))
-    screen.blit(tumo,(0,0))
-    pygame.display.update()
+    current_time = pygame.time.get_ticks()
+    if current_time > 1000:
+        seconds += 1
+        pygame.time.wait(1000)
+    if seconds == 60:
+        minutes += 1
+        seconds = 0 
+    if minutes == 60:
+        hours += 1
+        minutes = 0
+    if hours == 24:
+        seconds = 0
+        minutes = 0
+        hours = 0
     mx += sx
     my += sy
     if mx <= 0 or mx >= 1520:
         sx *= -1
     if my <=0 or my >= 880:
         sy *= -1
-    for i in range(60):
-        seconds += 1
-    for i in range(60):
-        if seconds == 60:
-            seconds = 0
-            minutes += 1
-    for i in range(24):
-        if minutes == 60:
-            minutes = 0
-            hours += 1
-        if hours == 24:
-            seconds = 0
-            minutes = 0
-            hours = 0
-    clock.tick(1)
+    tume = pygame.font.Font("C:/Users/zaydz/Desktop/coding/python/python programs/fonts/Sitka.ttc",50)
+    tumo = tume.render(f"hour : {hours}  minute : {minutes}  second : {seconds}",False,(237, 205, 187))
+    screen.blit(bg,(0,0))
+    screen.blit(mover,(mx,my))
+    screen.blit(tumo,(0,0))
+    pygame.display.update()
+    clock.tick(50)
