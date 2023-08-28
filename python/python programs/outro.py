84e8cd1e95dea32be358b6def61a67326e2de36d
mr.nokia <zaydzai@gmail.com>
2023-08-28 22:21:13 +0500

    sad

--- python/python programs/outro.py
+++
@@ -1,5 +0,0 @@
-import os
-from playsound import playsound
-sec = 12
-os.system(f"shutdown /s /t {sec}")
-playsound("C:\\Users\\zaydz\\Desktop\\coding\\python\\python programs\\sound effects\\outro.mp3")
